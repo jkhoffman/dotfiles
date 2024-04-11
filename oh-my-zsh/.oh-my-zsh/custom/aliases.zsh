9896ad2275b90ea58f306b50e3717e0bd51e9602
Jeffery Hoffman <jkhoffman@gmail.com>
2024-04-10 23:12:59 -0400

    Initial import

---
+++ oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,16 @@
+alias zshrc="nvim ~/.zshrc"
+alias ohmyzsh="nvim ~/.oh-my-zsh"
+
+alias n="nvim"
+alias c="clear"
+alias e="exit"
+alias l='eza'
+alias ll="eza -al --group-directories-first --icons"
+alias t="tmux"
+alias tn='(){ tmux new -s $1 }'
+alias p="pnpm"
+alias g="git"
+alias ga="git add -A"
+alias gs="git status -s"
+alias gc='(){ git commit -m $1 }'
+
